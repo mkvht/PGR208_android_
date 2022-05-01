b76e75aad14b80a89569cafb95699c1bfae6e842
Maria Tan <mkvht@outlook.com>
2022-05-01 21:53:09 +0200

    nesten fikest

--- app/src/main/java/com/example/pgr208_android_eksamen/fragments/LoadFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pgr208_android_eksamen.fragments
-
-import androidx.fragment.app.Fragment
-import com.example.pgr208_android_eksamen.R
-
-class LoadFragment : Fragment(R.layout.fragment_load_data)
